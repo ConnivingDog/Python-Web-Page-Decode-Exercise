86161361ec6f5d301339f227aa2b6b56c988e501
Ronan Lina <ronanlina@gmail.com>
2018-08-09 13:38:12 +0800

    find menu/selector, set file name method

--- main.py
+++ main.py
@@ -8,14 +8,22 @@ import io
 
 from bs4 import BeautifulSoup
 
-url = 'http://github.com'
+def SetURL():
+    user_url = input('Enter new URL http:// ')
+    user_url = 'http://' + user_url
+    return user_url
+
+def SetFileName():
+    user_file = input('Enter new file name : ')
+    return user_file + '.html'
+
+url = SetURL()
+filename = SetFileName()
 r = requests.get(url)
 r_html = r.text
-
-soup = BeautifulSoup(r_html)
+soup = BeautifulSoup(r_html, features='html.parser')
 
 def main():
-    SetURL()
     os.system('cls')
     PrintMenu()
     activity = int(input('Enter activity : '))
@@ -24,37 +32,49 @@ def main():
     main()
 
 def Prettify():
-    fo = open ('parsed.html','w+', encoding = 'utf-8')
+    fo = open (filename,'w+', encoding = 'utf-8')
     fo.write(soup.prettify())
     fo.close
-
-def SetURL():
-    url = str(input('Enter new URL : '))
-    return
 
 #Determines wich method to run
 def Selector(Activity):
     if Activity == 1:
         Prettify()
     elif Activity == 2:
-        pass
+        PrintFindMenu()
+        FmsActivity = int(input('Enter search type : '))
+        FindMenuSelector(FmsActivity)
     elif Activity == 3:
-        pass
+        SetURL()
     elif Activity == 4:
-        pass
+        SetFileName()
     elif Activity == 5:
         pass
     else:
         os.system('cls')
         quit()
-    
+
+def FindMenuSelector(FmsActivity):
+    if FmsActivity == 1:
+        pass
+    elif FmsActivity == 2:
+        pass
+    else:
+        main()
 
 def PrintMenu():
     print('[1]. Get HTML')
-    print('[2]. Custom Find')
+    print('[2]. Find')
     print('[3]. Set URL')
-    print('[4]. --')
+    print('[4]. Set File Name')
     print('[5]. --')
     print('[6]. Exit\n')
+
+def PrintFindMenu():
+    os.system('cls')
+    print('[1]. Tag')
+    print('[2]. File Type (.***)')
+    print('[3]. Back\n')
+
 
 main()
